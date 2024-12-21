6a99a4494ec9d0dd28af32b926e6e298be145b74
square-spade <41908243+square-spade@users.noreply.github.com>
2024-12-22 03:12:10 +1100

    Update coordinator.py

--- custom_components/ttlock/coordinator.py
+++ custom_components/ttlock/coordinator.py
@@ -172,7 +172,7 @@ class LockUpdateCoordinator(DataUpdateCoordinator[LockState]):
                     pass
 
             new_data.auto_lock_seconds = details.autoLockTime
-            new_data.lock_sound = details.lockSound == OnOff.on
+            new_data.lock_sound = details.lockSound
 
             new_data.passage_mode_config = await self.api.get_lock_passage_mode_config(
                 self.lock_id
