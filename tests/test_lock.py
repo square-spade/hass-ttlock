1497b9183f231063004dbca35ee469fa8416cbc6
square-spade <41908243+square-spade@users.noreply.github.com>
2024-12-30 00:37:00 +1100

    Delete tests directory

--- tests/test_lock.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.core import HomeAssistant
-
-
-async def test_lock_states(hass: HomeAssistant, mock_api_responses, component_setup):
-    """Test different lock states."""
-    mock_api_responses("default")
-    coordinator = await component_setup()
-    assert not coordinator.data.locked
