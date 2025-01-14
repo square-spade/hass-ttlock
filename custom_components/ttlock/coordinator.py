ff44158b1dc9e3686ac04102c0286b844283758e
Jonas Bergler <jonas@bergler.name>
2025-01-14 18:06:33 +1300

    pre-commit

--- custom_components/ttlock/coordinator.py
+++ custom_components/ttlock/coordinator.py
@@ -17,7 +17,7 @@ from homeassistant.util import dt
 
 from .api import TTLockApi
 from .const import DOMAIN, SIGNAL_NEW_DATA, TT_LOCKS
-from .models import Features, OnOff, PassageModeConfig, SensorState, State, WebhookEvent
+from .models import Features, PassageModeConfig, SensorState, State, WebhookEvent
 
 _LOGGER = logging.getLogger(__name__)
 
